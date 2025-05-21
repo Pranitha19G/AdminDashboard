d02553557868111f0092962169ff42420d37447b
Pranitha19G <gandrathpranitha@gmail.com>
2025-05-22 00:28:54 +0530

    first commit

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,15 +1,44 @@
-import React from "react";
-import ProductList from "../../Utils/ProductList";
+import React, { useState } from "react";
+import { ProductList } from "../../Utils/ProductList";
 import styles from "./Products.module.css";
 
 export default function Products() {
-  <div className={styles.Product}>
-    1223
-    {ProductList.map((val, i) => (
-      <div key={val.id} className={styles.products}>
-        <img src={val.products_image} alt="img" />
-        <div>{val.product_name}</div>
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(ProductList.length / 10);
+
+  const starting = (currentPage - 1) * 10;
+  const pages = ProductList.slice(starting, starting + 10);
+
+  const paginationfun = (pagenumber) => {
+    setCurrentPage(pagenumber);
+  };
+
+  return (
+    <>
+      <div className={styles.Product}>
+        {pages.map((val, i) => (
+          <div key={val.id} className={styles.products}>
+            <img src={val.products_image} alt="img" />
+            <div>{val.product_name}</div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>;
+      <div className={styles.pagination}>
+
+        <button onClick={() => setCurrentPage(currentPage>1?currentPage- 1: null)}>prev</button>
+
+        {[...Array(totalPages)].map((val, i) => (
+          <div
+            onClick={() => paginationfun(i + 1)}
+            className={`${styles.pagenumbers} ${
+              currentPage === i + 1 ? styles.numbers : ""
+            }`}
+          >
+            {i + 1}
+          </div>
+        ))}
+        <button onClick={() => setCurrentPage(currentPage + 1)}>next</button>
+      </div>
+    </>
+  );
 }
